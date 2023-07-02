083d70eb780d5c4b9004c4f563e22a3d2141980b
Iseo Choi <chxiseo@naver.com>
2023-07-02 22:28:09 +0900

    first commit

--- exam01/src/test/java/com/iseo/exam01/Exam01ApplicationTests.java
+++ exam01/src/test/java/com/iseo/exam01/Exam01ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.iseo.exam01;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+@SpringBootApplication
 class Exam01ApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(Exam01Application.class, args);
 	}
 
 }
